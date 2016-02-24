5c253d7e74dfc891fa5318c4ff4d556ff6854b16
liangjingzhan <fatzhan@163.com>
2016-02-24 10:38:48 +0800

    promise1

--- promise1/demo.js
+++ promise1/demo.js
@@ -1,3 +1,19 @@
-/**
- * Created by liangjz on 2/24/16.
- */
+var Promise = require('./q');
+
+function asyncJob1(param) {
+    var promise = Promise();
+    setTimeout(function monkey() {
+        var result = param + ' with job1';
+        promise.resolve(result);
+    }, 100);
+    return promise;
+}
+
+var job1 = asyncJob1('monkey');
+job1.then(function (result) {
+    console.log('we are done1:', result);
+});
+
+job1.then(function (result) {
+    console.log('we are done2:', result);
+});
--- promise1/q.js
+++ promise1/q.js
@@ -1,3 +1,20 @@
-/**
- * Created by liangjz on 2/24/16.
- */
+var Promise = function () {
+    var _pending = [], _result;
+
+    return {
+        resolve: function (result) {
+            _result = result;
+            if (_pending) {
+                for (var i = 0, l = _pending.length; i < l; i++) {
+                    _pending[i](_result);
+                }
+            }
+            _pending = null;
+        },
+        then: function (callback) {
+            _pending.push(callback);
+        }
+    }
+};
+
+module.exports = Promise;
